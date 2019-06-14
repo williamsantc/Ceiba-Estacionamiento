3d26c362d723fade0101fc30afbcaca174428c81
william.santos <william.santos@ceiba.com.co>
2019-06-14 17:16:55 -0500

    resulución de errores

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,1 @@
--- CARS
-insert into entry (id, licence_plate, vehicle_type, entry_time) values (101, 'QAR1243', 'CAR', '2019-06-10');
-
--- MOTORCYCLES
-insert into entry (id, licence_plate, vehicle_type, entry_time) values (121, 'QAR1240', 'MOTORCYCLE', '2019-06-12');
+insert into entry (id, licence_plate, vehicle_type, entry_time) values (102, 'SR1243', 'CAR', '2019-01-01');
